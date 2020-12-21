ac6da7d7ecee09ed0cad711163c33d52461fc9d4
Justintime4763 <alt.justin4763@gmail.com>
2020-12-20 23:37:10 -0500

    on_ready()

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,17 @@
-import os, discord
+import discord
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+import os
 
-print("Hello world!")
+TOKEN = ''
+BOT_PREFIX = '>'
+BOT_PLAYING = ''
+
+bot = commands.bot(command_prefix=BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print("Logged in as " + bot.user.name + "\n")
+    
+bot.run(TOKEN)
