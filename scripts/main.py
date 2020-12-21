f9d995a189b745fef35e2c4a81076214febb6a85
Justintime4763 <alt.justin4763@gmail.com>
2020-12-21 02:27:13 -0500

    Testing .gitignore to see if I can include token in project

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,25 @@
-import discord
+import discord, youtube_dl, os, time, sys
 from discord.ext import commands
 from discord.utils import get
-import youtube_dl
-import os
 
-TOKEN = ''
+#INITIALIZATION, SETTING TOKENS, PREFIXES ETC.
+try:
+    TOKEN = open("token.ini", "r").read()
+except:
+    print("File does not exist; use your own token by deleting this try/except statement and just setting token as"
+          + " your own token")
+    time.sleep(5)
+    sys.exit(1)
+    
 BOT_PREFIX = '>'
 BOT_PLAYING = ''
 
-bot = commands.bot(command_prefix=BOT_PREFIX)
+print(f"token is {TOKEN}") 
 
-@bot.event
-async def on_ready():
-    print("Logged in as " + bot.user.name + "\n")
-    
-bot.run(TOKEN)
+# bot = commands.bot(command_prefix=BOT_PREFIX)
+#  
+# @bot.event
+# async def on_ready():
+#     print("Logged in as " + bot.user.name + "\n")
+#      
+# bot.run(TOKEN)
