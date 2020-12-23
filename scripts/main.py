969dfd5f3895194d995a0059637b23b44d647ad4
Justintime4763 <justin.luque@Gmail.com>
2020-12-22 23:49:34 -0500

    Adding search and volume functionality; cleaned up messages

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
-import discord, youtube_dl, os, time, sys
+import discord, youtube_dl, os, time, sys, re
 from discord.ext import commands
 from discord.utils import get
+import urllib.request
 
 # INITIALIZATION, SETTING TOKENS, PREFIXES ETC.
 
@@ -20,6 +21,11 @@ def video_size(url):
         dictMeta = ydl.extract_info(url, download=False)
         return dictMeta['duration']
 
+def yt_firstresult(s):
+    link = urllib.request.urlopen('https://www.youtube.com/results?search_query=' 
+                                  + (s.replace(" ", "+")).replace("/", "\\"))
+    video_ids = re.findall(r"watch\?v=(\S{11})", link.read().decode())
+    return "https://www.youtube.com/watch?v=" + video_ids[0]
 
 src = os.path.abspath('../src/')
  
@@ -81,7 +87,7 @@ async def join(ctx):
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
+        print(f"[{ctx.guild}] Joined channel\n")
         
     await ctx.send(f"**Joined {channel}!** :wave: :speaking_head:")
 
@@ -99,7 +105,7 @@ async def leave(ctx):
         if channel is None:
             channel = voice.channel
         await voice.disconnect()
-        print(f"The bot has left {channel}")
+        print(f"[{ctx.guild}] Left channel\n")
         await ctx.send(f"**Left {channel}!** :wave:")
     else:
         print("Bot attempted to leave voice channel, was not in any to leave")
@@ -112,11 +118,13 @@ async def play(ctx, url: str):
     
     voice = get(bot.voice_clients, guild=ctx.guild)
     
-    if not voice and not voice.is_connected:
+    if not voice:
         try:
             await join(ctx)
         except:
             pass
+        
+    voice = get(bot.voice_clients, guild=ctx.guild)
     
     try:
         if os.path.isfile(os.path.join(src, "song.mp3")):
@@ -153,7 +161,7 @@ async def play(ctx, url: str):
     voice.source.volume = 1.0
      
     nname = name.rsplit("-", 2)
-    await ctx.send(f"**Playing** :notes: `{url}` in {voice.channel}!")
+    await ctx.send(f"**Playing** :notes: `{url}`** in {voice.channel}!**")
     print(f"Playing `{url}` **in {voice.channel}**")
 
 
@@ -193,5 +201,32 @@ async def stop(ctx):
     else:
         await ctx.send("**Nothing playing!** :shrug:")
 
+@bot.command(brief="Search for video to play", description="Searching for any youtube video to play")
+async def search(ctx, *args):
+    
+    keywords = " ".join(args)
+    
+    await ctx.send("**Querying youtube...**")
+    
+    link = yt_firstresult(keywords)
+    
+    await play(ctx, link)
         
+@bot.command(brief="Set volume for bot", description="Updates bot volume.")
+async def volume(ctx, vol: str):
+    
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    
+    if not voice:
+        await ctx.send("**Bot not in any voice channels** :shrug:")
+        return 
+
+    try:
+        if int(vol.strip("%"))/100 < 2.01:
+            voice.source.volume = int(vol.strip("%"))/100
+        else:
+            await ctx.send("**Volume too high... listening above 200% may damage your hearing**")
+    except:
+        await ctx.send("**Volume invalid** :shrug:")
+    
 bot.run(TOKEN)
